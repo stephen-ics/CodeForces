7977db3e990512bd0cae799c3e9557d10a4d547b
stephen-ics <stephenni1234@gmail.com>
2023-12-21 01:41:24 -0500

    P3 - Way Too Long Word

---
+++ Problem Set 100/Strings and Arrays/P3 - Way Too Long Words.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Stephen Ni on 2023-12-21.
+//
+
+//
+// Created by Stephen Ni on 2023-12-21.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        string word;
+        cin >> word;
+
+        int length = word.length();
+
+        if (length <= 10) {
+            cout << word << endl;
+        }
+        else {
+            cout << word[0] << word.length()-2 << word[word.length()-1] << endl;
+        }
+    }
+
+
+
+
+    return 0;
+}
