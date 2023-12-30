03e806530b69d0fcbc51a857f3b169b4429b5e88
stephen-ics <stephenni1234@gmail.com>
2023-12-29 19:37:22 -0500

    419B - Karen and Coffee Explanation

--- Practice Problems/Prefix Sum Arrays/419B - Karen and Coffee.cpp
+++ Practice Problems/Prefix Sum Arrays/419B - Karen and Coffee.cpp
@@ -34,7 +34,7 @@ int main() {
     }
 
     vector<int> p(200005, 0);
-    for(int i = 0; i < 200005; i++) {
+    for(int i = 1; i < 200005; i++) {
         p[i] = c[i - 1] + p[i - 1];
     }
 
@@ -47,3 +47,9 @@ int main() {
 
     return 0;
 }
+
+// the main takeaway for this problem is that when you have a range of values e.g 92 - 97, most of the time if you are looking for the amount of time each number shows up you don't have the loop through each number
+// you can increment beginning and decrement the ending value and all values that sum up to the 'k' threshold (appear more than k times) are valid
+// it can be thought of as 'layers' on intervals
+// the last part summation is more the "common prefix sum array" part, where the amount of valid numbers in a range is equivalent to the amount of valid numbers up to the upper bound subtracted by the amount of valid numbers up to the lowest bound
+// taking the summation is similar to taking the area a.k.a taking the interval, so if A is the accumulation of B and B is the accumulation of C, you can perceive an relationship of integrals/derivatives
